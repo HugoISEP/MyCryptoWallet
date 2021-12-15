7491dc28a00a2e080a3f70b55924891956db8a0a
hugo <hchemillier@juniorisep.com>
2021-12-15 12:04:56 +0100

    handle api credentials

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MyCryptoWallet
+# MyCryptoWallet  
+  
+## credentials  
+> The project use Binance API, so get your account (ask hugo for a referential link) and setup your api credentials
+> create a file apikey.properties in the root of the projet and add these two lines (don't forget the quotes):  
+ ```
+API_KEY="YOUR API KEY" 
+API_PASSWORD="YOUR API PASSWORD"
+```
--- app/src/main/java/com/example/mycryptowallet/configuration/AuthenticationInterceptor.kt
+++ app/src/main/java/com/example/mycryptowallet/configuration/AuthenticationInterceptor.kt
@@ -1,6 +1,7 @@
 package com.example.mycryptowallet.configuration
 
 import android.util.Log
+import com.example.mycryptowallet.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -11,7 +12,7 @@ internal class AuthenticationInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest: Request = chain.request()
         Log.d("AuthenticationIntercept", "good")
-        originalRequest.headers.newBuilder().add("API_KEY", "API_VALUE")
+        originalRequest.headers.newBuilder().add(BuildConfig.API_KEY, BuildConfig.API_PASSWORD)
         return chain.proceed(originalRequest)
     }
 
